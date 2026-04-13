a427bac3681462ba8e79a12a9b08a6641a79269c
smoothjazz18n94 <gorjessbrown@gmail.com>
2026-04-13 04:40:17 -1200

    update html

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-console.log("✅ User model loaded");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -8,5 +8,18 @@ const userSchema = new mongoose.Schema({
   accountNumber: String,
   balance: { type: Number, default: 0 },
 });
+
+// 🔐 HASH PASSWORD BEFORE SAVE
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// 🔐 COMPARE PASSWORD
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
